588b725cedc0e37a8d32117ee66ff4809eb73386
tamil-12 <tamilselvan92485@gmail.com>
2026-02-19 12:30:16 +0530

    frontend upload

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -11,8 +11,7 @@ const Home = () => {
   const [products, setProducts] = useState([]);
   const [bestSelling, setBestSelling] = useState(null);
   const navigate = useNavigate();
-  // const BASE_URL = "http://localhost:2000";
-  const BASE_URL = "https://golden-grains-backend.onrender.com";
+  const BASE_URL = "http://localhost:2000";
 
   useEffect(() => {
     axios.get(`${BASE_URL}/api/carousel`)
